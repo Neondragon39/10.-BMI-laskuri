9aa0e38f74a279bf35f85a4781e01b839220ad62
Neondragon39 <vlad.jeskin@gmail.com>
2023-04-19 06:14:03 +0300

    muutos

--- 10. BMI-laskuri/Form1.Designer.cs
+++ 10. BMI-laskuri/Form1.Designer.cs
@@ -28,12 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PainoTB = new System.Windows.Forms.TextBox();
+            this.PituusTB = new System.Windows.Forms.TextBox();
+            this.LaskeBT = new System.Windows.Forms.Button();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.KuvausLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anna paino:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Anna pituus:";
+            // 
+            // PainoTB
+            // 
+            this.PainoTB.Location = new System.Drawing.Point(135, 12);
+            this.PainoTB.Name = "PainoTB";
+            this.PainoTB.Size = new System.Drawing.Size(100, 23);
+            this.PainoTB.TabIndex = 2;
+            // 
+            // PituusTB
+            // 
+            this.PituusTB.Location = new System.Drawing.Point(135, 51);
+            this.PituusTB.Name = "PituusTB";
+            this.PituusTB.Size = new System.Drawing.Size(100, 23);
+            this.PituusTB.TabIndex = 3;
+            this.PituusTB.TextChanged += new System.EventHandler(this.PituusTB_TextChanged);
+            // 
+            // LaskeBT
+            // 
+            this.LaskeBT.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LaskeBT.Location = new System.Drawing.Point(12, 91);
+            this.LaskeBT.Name = "LaskeBT";
+            this.LaskeBT.Size = new System.Drawing.Size(223, 41);
+            this.LaskeBT.TabIndex = 4;
+            this.LaskeBT.Text = "Laske  painoindeksi";
+            this.LaskeBT.UseVisualStyleBackColor = true;
+            this.LaskeBT.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.VastausLB.Location = new System.Drawing.Point(12, 157);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(76, 25);
+            this.VastausLB.TabIndex = 5;
+            this.VastausLB.Text = "Vastaus";
+            this.VastausLB.Visible = false;
+            this.VastausLB.Click += new System.EventHandler(this.VastausLB_Click);
+            // 
+            // KuvausLB
+            // 
+            this.KuvausLB.AutoSize = true;
+            this.KuvausLB.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.KuvausLB.Location = new System.Drawing.Point(12, 195);
+            this.KuvausLB.Name = "KuvausLB";
+            this.KuvausLB.Size = new System.Drawing.Size(72, 25);
+            this.KuvausLB.TabIndex = 6;
+            this.KuvausLB.Text = "Kuvaus";
+            this.KuvausLB.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(250, 253);
+            this.Controls.Add(this.KuvausLB);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.LaskeBT);
+            this.Controls.Add(this.PituusTB);
+            this.Controls.Add(this.PainoTB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = " BMI-laskuri";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox PainoTB;
+        private TextBox PituusTB;
+        private Button LaskeBT;
+        private Label VastausLB;
+        private Label KuvausLB;
     }
 }
--- 10. BMI-laskuri/Form1.cs
+++ 10. BMI-laskuri/Form1.cs
@@ -6,5 +6,59 @@ namespace _10._BMI_laskuri
         {
             InitializeComponent();
         }
+
+        private void PituusTB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VastausLB_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double paino = 0, pituus = 0, painoindeksi;
+            paino = Convert.ToDouble(PainoTB.Text);
+            pituus = Convert.ToDouble(PituusTB.Text);
+            painoindeksi = Math.Round(paino / (pituus * pituus), 2);
+            if (painoindeksi < 18.5)
+            {
+                VastausLB.Text = "Painoindeksisi on: " + painoindeksi;
+                VastausLB.ForeColor = Color.Blue;
+                KuvausLB.Text = "Alipaino";
+                KuvausLB.ForeColor = Color.Blue;
+                VastausLB.Visible = true;
+                KuvausLB.Visible = true;
+            }
+            else if (painoindeksi < 25)
+            {
+                VastausLB.Text = "Painoindeksisi on: " + painoindeksi;
+                VastausLB.ForeColor = Color.Green;
+                KuvausLB.Text = "Normaalipaino";
+                KuvausLB.ForeColor = Color.Green;
+                VastausLB.Visible = true;
+                KuvausLB.Visible = true;
+            }
+            else if (painoindeksi < 40)
+            {
+                VastausLB.Text = "Painoindeksisi on: " + painoindeksi;
+                VastausLB.ForeColor = Color.DarkOrange;
+                KuvausLB.Text = "Ylipaino";
+                KuvausLB.ForeColor = Color.DarkOrange;
+                VastausLB.Visible = true;
+                KuvausLB.Visible = true;
+            }
+            else 
+            {
+                VastausLB.Text = "Painoindeksisi on: " + painoindeksi;
+                VastausLB.ForeColor = Color.Red;
+                KuvausLB.Text = "Huommattava ylipaino";
+                KuvausLB.ForeColor = Color.Red;
+                VastausLB.Visible = true;
+                KuvausLB.Visible = true;
+            }
+        }
     }
 }
